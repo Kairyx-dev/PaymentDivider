9b2c1e4e198838535dd8b458ebd9236a36fbaae4
Kairyx <rhkstn0303@gmail.com>
2025-11-18 10:24:15 +0900

    [DEPLOY] Version 0.1.1

--- architecture/app/build.gradle.kts
+++ architecture/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "org.specter.paymentdivider"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
